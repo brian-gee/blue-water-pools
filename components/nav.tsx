eb3c341ba524c68ba4ca29c48bc14441f656e8db
brian-gee <brian86753099@gmail.com>
2022-11-10 13:59:43 -0500

    creating layout for all pages

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,1 +1,16 @@
-export default function Nav() {}
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav className="p-10 mb-12 flex justify-between">
+      <h1 className="text-xl">SK Cleaning</h1>
+      <ul className="flex items-center ">
+        <Link href="/admin">
+          {/* <li className="pr-4">{user ? "Add Customer" : ""}</li> */}
+        </Link>
+        {/* <li className="pr-4">{user ? user.displayName : ""}</li> */}
+        <li>{/* <SignOut /> */}</li>
+      </ul>
+    </nav>
+  );
+}
