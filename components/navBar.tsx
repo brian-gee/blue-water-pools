0f8b7787adc03bff96d04090ad514b44cc97e1f1
brian-gee <brian86753099@gmail.com>
2022-11-18 02:09:32 -0500

    refactor

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -8,28 +8,6 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import defaultPic from '../public/defaultPic.jpg';
 
-// function Nav() {
-// 	return (
-// 		<nav className="p-10 mb-12 flex justify-between ">
-// 			<Link href="/">
-// 				<h1 className="text-xl">SK Cleaning</h1>
-// 			</Link>
-// 			<ul className="flex items-center ">
-// 				<Link href="/">
-// 					<li className="pr-4">Home</li>
-// 				</Link>
-// 				<Link href="/manage">
-// 					<li className="pr-4">{user ? 'Manage Customers' : ''}</li>
-// 				</Link>
-// 				<li className="pr-4">{user ? user.displayName : ''}</li>
-// 				<li>
-// 					<button onClick={logOut}>{user ? 'Sign Out' : ''}</button>
-// 				</li>
-// 			</ul>
-// 		</nav>
-// 	);
-// }
-
 const navigation = [
 	{ name: 'Dashboard', href: '/', current: true },
 	{ name: 'Team', href: '#', current: false },
