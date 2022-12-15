22383cd81966d5357231f9187e4242c1863edc86
brian-gee <brian86753099@gmail.com>
2022-12-15 00:42:50 -0500

    removing user requirement to view grid and data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,18 @@
-import Head from "next/head";
-import SignIn from "../components/signIn";
-import CustomerGrid from "../components/customerGrid"
-import { AuthContextProvider } from "../firebase/initAuth";
+import Head from 'next/head';
+import CustomerGrid from '../components/customerGrid';
 
 export default function Home() {
-  const { user } = AuthContextProvider();
+	return (
+		<div className="">
+			<Head>
+				<title>Blue Water Pools</title>
+				<meta name="description" content="The pool service for you" />
+				<link rel="icon" href="../public/favicon.svg" />
+			</Head>
 
-  return (
-    <div className="">
-      <Head>
-        <title>Blue Water Pools</title>
-        <meta
-          name="description"
-          content="The pool service for you"
-        />
-        <link rel="icon" href="../public/favicon.svg" />
-      </Head>
-
-      <main className="px-10">
-        <div className="p-10">{user ? <CustomerGrid/> : <SignIn />}</div>
-      </main>
-    </div>
-  );
+			<main className="px-10">
+				<div className="p-10">{<CustomerGrid />}</div>
+			</main>
+		</div>
+	);
 }
