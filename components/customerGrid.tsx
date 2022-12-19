b277b139433a916a54dfe60b7c935a788daf9075
brian-gee <brian86753099@gmail.com>
2022-12-19 01:49:22 -0500

    working to get trash and edit modals to edit data

--- components/customerGrid.tsx
+++ components/customerGrid.tsx
@@ -1,20 +1,17 @@
 import { dbRef } from '../firebase/initFirebase';
 import { onValue } from 'firebase/database';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import {
-  DataGrid,
-	GridColDef,
+	DataGrid,
 	GridValueGetterParams,
-  GridActionsCellItem,
-  GridRowId,
-  GridColumns,
-	GridToolbar
+	GridActionsCellItem,
+	GridColumns,
+	GridToolbar,
 } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SecurityIcon from '@mui/icons-material/Security';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
-
 
 const darkTheme = createTheme({
 	palette: {
@@ -27,67 +24,7 @@ const dollarUS = Intl.NumberFormat('en-US', {
 	currency: 'USD',
 });
 
-
-
-
-const columns: GridColDef[] = [
-	// { field: 'id', headerName: 'ID', width: 90 },
-	{
-		field: 'first_name',
-		headerName: 'First name',
-		width: 150,
-		editable: true,
-	},
-	{
-		field: 'last_name',
-		headerName: 'Last name',
-		width: 150,
-		editable: true,
-	},
-	{
-		field: 'email',
-		headerName: 'Email',
-		width: 150,
-		editable: true,
-	},
-	{
-		field: 'address',
-		headerName: 'Address',
-		width: 150,
-		editable: true,
-	},
-	{
-		field: 'invoice',
-		headerName: 'Invoice',
-		type: 'number',
-		width: 110,
-		editable: true,
-		valueGetter: (params: GridValueGetterParams) =>
-			`${dollarUS.format(params.row.invoice)}`,
-	},
-	// {
-	// 	field: 'actions',
-	// 	type: 'actions',
-	// 	width: 80,
-	// 	getActions: (params) => [
-	// 		<GridActionsCellItem
-	// 			icon={<DeleteIcon />}
-	// 			label="Delete"
-	// 			onClick={testClick}
-	// 		/>,
-	// 		<GridActionsCellItem
-	// 			icon={<SecurityIcon />}
-	// 			label="Toggle Admin"
-	// 			showInMenu
-	// 		/>,
-	// 		<GridActionsCellItem
-	// 			icon={<FileCopyIcon />}
-	// 			label="Duplicate User"
-	// 			showInMenu
-	// 		/>,
-	// 	],
-	// },
-];
+const confirmDelete = (id: string) => {};
 
 export default function CustomerGrid() {
 	const [rows, setRows] = useState([]);
@@ -96,6 +33,94 @@ export default function CustomerGrid() {
 			setRows(snapshot.val());
 		});
 	}, []);
+
+	const deleteUser = useCallback(
+		(id: string) => () => {
+			console.log(id);
+		},
+		[]
+	);
+
+	const toggleAdmin = useCallback(
+		(id: string) => () => {
+			console.log(id);
+		},
+		[]
+	);
+
+	const duplicateUser = useCallback(
+		(id: string) => () => {
+			console.log(id);
+		},
+		[]
+	);
+
+	const columns = useMemo<GridColumns>(
+		() => [
+			// { field: 'id', headerName: 'ID', width: 90 },
+			{
+				field: 'first_name',
+				headerName: 'First name',
+				width: 150,
+				editable: true,
+			},
+			{
+				field: 'last_name',
+				headerName: 'Last name',
+				width: 150,
+				editable: true,
+			},
+			{
+				field: 'email',
+				headerName: 'Email',
+				width: 150,
+				editable: true,
+			},
+			{
+				field: 'address',
+				headerName: 'Address',
+				width: 150,
+				editable: true,
+			},
+			{
+				field: 'invoice',
+				headerName: 'Invoice',
+				type: 'number',
+				width: 110,
+				editable: false,
+				valueGetter: (params: GridValueGetterParams) =>
+					`${dollarUS.format(params.row.invoice)}`,
+			},
+			{
+				field: 'actions',
+				type: 'actions',
+				width: 80,
+				getActions: (params) => [
+					<GridActionsCellItem
+						key={params.row.id}
+						icon={<DeleteIcon />}
+						label="Delete"
+						onClick={deleteUser(params.row.id)}
+					/>,
+					<GridActionsCellItem
+						key={params.row.id}
+						icon={<SecurityIcon />}
+						label="Toggle Admin"
+						onClick={toggleAdmin(params.row.id)}
+						showInMenu
+					/>,
+					<GridActionsCellItem
+						key={params.row.id}
+						icon={<FileCopyIcon />}
+						label="Duplicate User"
+						onClick={duplicateUser(params.row.id)}
+						showInMenu
+					/>,
+				],
+			},
+		],
+		[deleteUser, toggleAdmin, duplicateUser]
+	);
 
 	return (
 		<ThemeProvider theme={darkTheme}>
@@ -109,7 +134,7 @@ export default function CustomerGrid() {
 					checkboxSelection
 					disableSelectionOnClick
 					experimentalFeatures={{ newEditingApi: true }}
-					className=' bg-gray-900 border-slate-300 border-2'
+					className=" bg-gray-900 border-slate-300 border-2"
 				/>
 			</div>
 		</ThemeProvider>
