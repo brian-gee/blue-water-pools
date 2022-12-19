3ae566c7c2fba12ebf85ee575bce80fb2b56e006
brian-gee <brian86753099@gmail.com>
2022-12-19 00:03:34 -0500

    trying to fix favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,11 @@ export default function Home() {
 	return (
 		<div className="">
 			<Head>
-				<title>Blue Water Pools</title>
+				<title>Blue Water Pool</title>
 				<meta name="description" content="The pool service for you" />
-				<link rel="icon" href="../public/favicon.svg" />
+				<link rel="icon" href="../public/favicon.ico" />
 			</Head>
+
 
 			<main className="px-10">
 				<div className="p-10">{<CustomerGrid />}</div>
